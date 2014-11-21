4bb3eed0377f076b05e10654ae0eb5909fdefb42
Fasih Awan <fasihawan@gmail.com>
2014-11-21 00:09:01 -0500

    Implemented initial Watcard code
    The future pointer work needs to be done collaboratively so we
    know what exactly needs to be done on that end. Other than that,
    since it's simply an object, all we need for now is the basic mutators
    and getter.

--- a6watcard.cc
+++ a6watcard.cc
@@ -1,1 +1,20 @@
 #include "a6watcard.h"
+
+WATCard::WATCard() {
+    watcardBalance = 0;
+}
+
+void WATCard::deposit( unsigned int amount ) {
+    watcardBalance += amount;
+}
+
+void WATCard::withdraw( unsigned int amount ) {
+    // Since the machine will be withdrawing money, it can first
+    // call getBalance() to determine if card has sufficient funds
+    // so we don't have to ever go negative
+    watcardBalance -= amount;
+}
+
+unsigned int getBalance() {
+    return watcardBalance;
+}
