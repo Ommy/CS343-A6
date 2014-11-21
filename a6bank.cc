d3e667a94b43128e779d938dd41d4efca50bf8ee
Fasih Awan <fasihawan@gmail.com>
2014-11-21 00:06:10 -0500

    Implemented Bank
    Have n condition locks and an array of n accounts. Since it's a monitor
    we don't have to worry about barging.

--- a6bank.cc
+++ a6bank.cc
@@ -1,2 +1,26 @@
 #include "a6bank.h"
 
+Bank::Bank( unsigned int numStudents ) : numStudents(numStudents) {
+    accountCheck = new uCondition[numStudents];
+    bankAccount = new unsigned int [numStudents];
+    for (unsigned int i = 0; i < numStudents; i++) {
+        bankAccount[i] = 0;
+    }
+}
+
+void Bank::deposit( unsigned int id, unsigned int amount ) {
+    bankAccount[id] += amount;
+    accountCheck[id].signal();    
+}
+
+void Bank::withdraw( unsigned int id, unsigned int amount ) {
+    while (bankAccount[id] < amount) {
+        accountCheck[id].wait();
+    }
+    bankAccount[id] -= amount;
+}
+
+Bank::~Bank(){
+    delete[] bankAccount;
+    delete[] accountCheck;
+}
