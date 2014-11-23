1283a064574e36dc5552dfd146a22ac7fe8b40c7
Victor Lai <lai.victor.vl@gmail.com>
2014-11-23 00:18:40 -0500

    Creating the BottlingPlant.

--- a6bottlingplant.h
+++ a6bottlingplant.h
@@ -2,16 +2,29 @@
 #define _A6_BOTTLING_PLANT_H__
 
 #include "a6nameserver.h"
+#include "a6vendingmachine.h"
 
 _Cormonitor Printer;
+_Task Truck;
 
 _Task BottlingPlant {
     void main();
+
+    Printer& printer;
+    NameServer& nameServer;
+    const unsigned int numberOfVendingMachines;
+    const unsigned int maxShippedPerFlavour;
+    const unsigned int maxStockPerFlavour;
+    const unsigned int timeBetweenShipments;
+
+    Truck* truck;
+    unsigned int shipment[VendingMachine::NUMBER_OF_FLAVOURS];
+    bool shuttingDown;
+
   public:
     _Event Shutdown {};
-    BottlingPlant( Printer &prt, NameServer &nameServer, unsigned int numVendingMachines,
-                 unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
-                 unsigned int timeBetweenShipments );
+    BottlingPlant( Printer &prt, NameServer &nameServer, unsigned int numberOfVendingMachines, unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour, unsigned int timeBetweenShipments );
+    ~BottlingPlant();
     void getShipment( unsigned int cargo[] );
 };
 
