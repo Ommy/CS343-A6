637c4c4d716e57d49558d0a08dcf5cce4366c238
Fasih Awan <fasihawan@gmail.com>
2014-11-23 01:33:58 -0500

    Implemented NameServer
    Might need to update the main a bit for the _Accept clauses, but for now
    I think its fine.

--- a6nameserver.h
+++ a6nameserver.h
@@ -2,16 +2,27 @@
 #define _A6_NAME_SERVER
 
 #include "a6vendingmachine.h"
+#include <map>
 
 _Cormonitor Printer;
 
 _Task NameServer {
-    void main();
   public:
     NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
+    ~NameServer();
     void VMregister( VendingMachine *vendingmachine );
     VendingMachine *getMachine( unsigned int id );
     VendingMachine **getMachineList();
+  private:
+    void main();
+    unsigned int numberOfStudents;
+    unsigned int numberOfVendingMachines;
+    unsigned int registeredMachine;
+    Printer * printer;
+    VendingMachine ** machines;
+    std::map<unsigned int, VendingMachine*> machineAssignment;
+    std::map<unsigned int, unsigned int> studentsCurrentMachine;
+
 };
 
 #endif
