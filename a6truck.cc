44f2c2cd5cf32fd42ad9296c44db780985686794
Victor Lai <lai.victor.vl@gmail.com>
2014-11-23 00:18:40 -0500

    Creating the BottlingPlant.

--- a6truck.cc
+++ a6truck.cc
@@ -1,1 +1,13 @@
 #include "a6truck.h"
+#include "a6printer.h"
+#include "a6nameserver.h"
+#include "a6bottlingplant.h"
+
+Truck::Truck( Printer &prt, NameServer &nameServer, BottlingPlant &plant,
+           unsigned int numVendingMachines, unsigned int maxStockPerFlavour ) {
+
+}
+
+void Truck::main() {
+    
+}
