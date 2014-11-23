422a0074c53b519d2eadc64e2a0c51c609762ec3
Fasih Awan <fasihawan@gmail.com>
2014-11-22 19:15:50 -0500

    Implemented Printer
    Utilized 4 maps that provided different functionality. The kindIndex simply gave the index of the
    column which the print statement is called for. The other 3 maps simply store the information, utilizing
    1 or 2 pairs inside the map. I wasn't sure if if the state was Finished we first print out the Finished
    only with the "..." and then print out all the other stuff or print it at once. Right now im using the latter
    as the assumption.

--- a6printer.cc
+++ a6printer.cc
@@ -2,3 +2,137 @@
 
 #include <iostream>
 #include <sstream>
+#include <string>
+
+Printer::Printer(unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers) {
+    numOfStudents = numStudents;
+    numOfVendingMachines = numVendingMachines;
+    numOfCouriers = numCouriers;
+    int totalColumnCount = 0;
+
+    kindIndex[Parent] = totalColumnCount;
+    std::cout << "Parent\t";
+    totalColumnCount++;
+
+    kindIndex[WATCardOffice] = totalColumnCount;
+    std::cout << "WATOff\t";
+    totalColumnCount++;
+
+    kindIndex[NameServer] = totalColumnCount;
+    std::cout << "Names\t";
+    totalColumnCount++;
+
+    kindIndex[Truck] = totalColumnCount;
+    std::cout << "Truck\t";
+    totalColumnCount++;
+
+    kindIndex[BottlingPlant] = totalColumnCount;
+    std::cout << "Plant\t";
+    totalColumnCount++;
+
+    kindIndex[Student] = totalColumnCount;
+    for (unsigned int i = 0; i < numOfStudents; i++) {
+        std::cout << "Stud" << i << "\t";
+        totalColumnCount++;
+    }
+
+    kindIndex[Vending] = totalColumnCount;
+    for (unsigned int i = 0; i < numOfVendingMachines; i++) {
+        std::cout << "Mach" << i << "\t";
+        totalColumnCount++;
+    }
+
+    kindIndex[Courier] = totalColumnCount;
+    for (unsigned int i = 0; i < numOfCouriers; i++) {
+        std::cout << "Cour" << i << "\t";
+        totalColumnCount++;
+    }
+
+    totalColumns = totalColumnCount;
+    std::cout << std::endl;
+
+    for (int i = 0; i < totalColumnCount; i++) {
+        std::cout << "*******" << "\t";
+    }
+
+
+}
+
+void Printer::print( Kind kind, char state ) {
+    print(kind, 0, state);
+}
+
+void Printer::print( Kind kind, char state, int value1 ) {
+    print(kind, 0, state, value1);
+}
+
+void Printer::print( Kind kind, char state, int value1, int value2 ) {
+    print(kind, 0, state, value1, value2);
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state ) {
+    lid += kindIndex[kind];
+    if (singleState.find(lid) != singleState.end()){
+        if (state == 'F') {
+            printEverything("...");
+        } else {
+            printEverything("");
+        }
+    }
+
+    singleState[lid] = state;
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state, int value1 ) {
+    lid += kindIndex[kind];
+    if (stateWithValue.find(lid) != stateWithValue.end()){
+        if (state == 'F') {
+            printEverything("...");
+        } else {
+            printEverything("");
+        }
+    }
+
+    stateWithValue[lid] = std::make_pair(state, value1);
+}
+
+void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ) {
+    lid += kindIndex[kind];
+    if (stateWithValues.find(lid) != stateWithValues.end()) {
+        if (state == 'F') {
+            printEverything("...");
+        } else {
+            printEverything("");
+        }
+    }
+
+    stateWithValues[lid] = std::make_pair(state, std::make_pair(value1, value2));
+}
+
+void Printer::printEverything(std::string placeholder){
+    for (int i = 0; i < totalColumns; i++) {
+        if (singleState.find(i) != singleState.end()) {
+            std::cout << singleState[i] << "\t";
+        } else if (stateWithValue.find(i) != stateWithValue.end()) {
+            std::cout << stateWithValue[i].first << stateWithValue[i].second << "\t";
+        } else if (stateWithValues.find(i) != stateWithValues.end()) {
+            std::cout << stateWithValues[i].first << stateWithValues[i].second.first << stateWithValues[i].second.second << "\t";
+        } else {
+            std::cout << placeholder << "\t";
+        }
+    }
+    std::cout << std::endl;
+    flushInfo();
+}
+
+void Printer::flushInfo(){
+    kindIndex.clear();
+    singleState.clear();
+    stateWithValue.clear();
+    stateWithValues.clear();
+}
+
+Printer::~Printer(){
+
+}
+
--- a6printer.h
+++ a6printer.h
@@ -1,9 +1,10 @@
 #ifndef _A6_PRINTER_H__
 #define _A6_PRINTER_H__
 
+#include <map>
+
 _Cormonitor Printer {
-    void main();
-    const unsigned int numberOfVoters;
+    // void main();
 public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
@@ -14,6 +15,18 @@ public:
     void print( Kind kind, unsigned int lid, char state );
     void print( Kind kind, unsigned int lid, char state, int value1 );
     void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+private:
+    void printEverything(std::string);
+    void flushInfo();
+    unsigned int numOfStudents;
+    unsigned int numOfVendingMachines;
+    unsigned int numOfCouriers;
+    int totalColumns;
+
+    std::map<Kind, unsigned int> kindIndex;
+    std::map<unsigned int, char> singleState;
+    std::map<unsigned int, std::pair<char, int> > stateWithValue;
+    std::map<unsigned int, std::pair<char, std::pair<int, int> > > stateWithValues;
 };
 
 #endif
