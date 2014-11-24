eeea4598ea616d4bef035ea1a6927b470d508a02
Fasih Awan <fasihawan@gmail.com>
2014-11-23 23:45:26 -0500

    Partially finished WATCardOffice
    There may be some additional work that needs to be done once we implement Student, but for now
    the functionality looks fine.

--- a6watcardoffice.cc
+++ a6watcardoffice.cc
@@ -1,21 +1,84 @@
 #include "a6watcardoffice.h"
+#include "a6main.h"
+#include "utilities.h"
 
-WATCardOffice::WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers ) {
+WATCardOffice::WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers) : printer(prt), bank(bank), numberOfCouriers(numCouriers) {
+    for (unsigned int i = 0; i < numberOfCouriers; i++) {
+        couriers.push_back(new Courier(prt, bank, *this));
+    }
+}
 
+WATCardOffice::~WATCardOffice() {
+    for (unsigned int i = 0; i < couriers.size(); i++) {
+        delete couriers[i];
+    }
+
+}
+
+WATCard::FWATCard WATCardOffice::createJob(unsigned int sid, unsigned int amount, WATCard *card) {
+    WATCardOffice::Args arg(sid, amount, card);    
+
+    WATCardOffice::Job * job = new WATCardOffice::Job(arg);
+    jobQueue.push_back(job);
+    jobLock.signal();
+    return job->result;
 }
 
 WATCard::FWATCard WATCardOffice::create( unsigned int sid, unsigned int amount ) {
-
+    return createJob(sid, amount, NULL);
 }
 
 WATCard::FWATCard WATCardOffice::transfer( unsigned int sid, unsigned int amount, WATCard *card ) {
-
+    return createJob(sid, amount, card);
 }
 
 WATCardOffice::Job * WATCardOffice::requestWork() {
-
+    while (jobQueue.empty()){
+        jobLock.wait();
+    }
+    return poll_front(jobQueue);
 }
 
 void WATCardOffice::main() {
+    while (true) {
+        _Accept(~WATCardOffice) {
+            break;
+        } or _Accept(create) {
+
+        } or _Accept(transfer) {
+
+        } or _Accept(requestWork) {
+
+        }
+    }
+}
+
+void WATCardOffice::Courier::main() {
+    while (true) {
+        _Accept( ~Courier ) {
+            break;
+        } _Else {
+            WATCardOffice::Job * job = office.requestWork();
+            Args jobArgs = job->args;
+
+            WATCard* card = jobArgs.card;
+            if (jobArgs.card == NULL) {
+                card = new WATCard();
+            }
+
+            bank.withdraw(jobArgs.sid, jobArgs.amount);
+            card->deposit(jobArgs.amount);
+            if (mprng(5) == 0) {
+                delete card;
+                job->result.exception(new WATCardOffice::Lost());
+            } else {
+                job->result.delivery(card);
+            }
+        }
+    }
+}
+
+WATCardOffice::Courier::Courier(Printer &prt, Bank &bank, WATCardOffice& office)
+: printer(prt), bank(bank), office(office) {
 
 }
--- a6watcardoffice.h
+++ a6watcardoffice.h
@@ -3,12 +3,18 @@
 
 #include "a6bank.h"
 #include "a6watcard.h"
+#include <vector>
+#include <deque>
+#include <map>
 
 _Cormonitor Printer;
 
 _Task WATCardOffice {
     struct Args {
-
+        Args(unsigned int sid, unsigned amount, WATCard * card) : sid(sid), amount(amount), card(card){}
+        unsigned int sid;
+        unsigned int amount;
+        WATCard * card;
     };
 
     struct Job {                           // marshalled arguments and return future
@@ -18,16 +24,35 @@ _Task WATCardOffice {
     };
     
     _Task Courier { 
+      public:
+        Courier(Printer &prt, Bank &bank, WATCardOffice &office);
+      private:
+        void main();
 
+        Printer & printer;
+        Bank & bank;
+        WATCardOffice & office;
     };                 // communicates with bank
 
     void main();
   public:
     _Event Lost {};                        // lost WATCard
     WATCardOffice( Printer &prt, Bank &bank, unsigned int numCouriers );
+    ~WATCardOffice();
     WATCard::FWATCard create( unsigned int sid, unsigned int amount );
     WATCard::FWATCard transfer( unsigned int sid, unsigned int amount, WATCard *card );
     Job *requestWork();
+  private:
+    WATCard::FWATCard createJob(unsigned int sid, unsigned int amount, WATCard* card);
+
+    Printer & printer;
+    Bank & bank;
+    unsigned int numberOfCouriers;
+    std::vector<Courier*> couriers;
+    std::deque<WATCardOffice::Job*> jobQueue;
+    uCondition jobLock;
+    std::map<unsigned int, WATCard*> studentsWatcard;
+
 };
 
 #endif
